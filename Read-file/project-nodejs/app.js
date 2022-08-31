8130bb47494591548d12c3fed1ac38ed4c7e6470
lequocthong29 <110148171+lequocthong29@users.noreply.github.com>
2022-08-31 07:59:42 +0700

    Read-file

--- Read-file/project-nodejs/app.js
+++ Read-file/project-nodejs/app.js
@@ -1,21 +1,33 @@
+const bodyParser = require('body-parser') 
+
 const express = require('express')
 const fs = require('fs');
 const path = require('path');
-const bodyParser = require('body-parser') 
+const internal = require('stream');
+
 const savefile = require('./base64')
 const app = express()
 const port = 3000
 
-app.use(express.json());    
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(express.json());    
 
 
 app.post('/files', (req, res) => {
-    //data.push(req.body.base64data);
+    //luu file
+    var a = savefile.file('testt.pdf');
+    if(a == 1){
+        
+    }else{
+        // gettoken
+        var url = 'https://rms.efy.com.vn/clients/login';
+        savefile.getTokenEfy(url,'rp_test','rp_test','RP_TEST');
+        
+    }
     
-    savefile.DecodePDF(savefile.EncodePDF('test.pdf'), 'C:/Users/Gibiop/Desktop/demo.pdf');
+       
+    //savefile.DecodePDF(req.body.base64data, 'C:/Users/Gibiop/Desktop/demo1.pdf');
 })
 
   // fs.writeFile(path.join(__dirname,'data.txt'),req.body.base64data,(err)=>{
--- Read-file/project-nodejs/base64.js
+++ Read-file/project-nodejs/base64.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
+const fs = require('fs');
 const pdf2base64 = require('pdf-to-base64');
-const base64 = require('base64topdf');
+
 
 function EncodePDF(path){
     let encodedPdf = base64.base64Encode(path);
@@ -12,6 +13,47 @@ function DecodePDF(enc, path){
 }
 
 
+function file(path){
+    try {
+        if (fs.existsSync(path)) {
+            return 1;
+        }else{
+            return 0;
+        }
+    } 
+    catch(err) {
+            
+    }
+}
+
+
+function getTokenEfy(path,username, password, rpCode){
+    var request = require('request');
+    var options = {
+        'method': 'POST',
+        'url': path,
+        'headers': {
+            'Content-Type': 'application/json'
+    },
+        body: JSON.stringify({
+            "username": username,
+            "password": password,
+            "rpCode": rpCode
+        })
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+    });
+}
+
+
+
+
 module.exports.EncodePDF = EncodePDF;
 
 module.exports.DecodePDF = DecodePDF;
+
+module.exports.file = file;
+
+module.exports.getTokenEfy = getTokenEfy;
